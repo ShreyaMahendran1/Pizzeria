435587204ab2b1403dd1e242a2844346902bb8b4
ShreyaMahendran1 <84270576+ShreyaMahendran1@users.noreply.github.com>
2021-08-31 10:23:53 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ menu_list_array.sort();
         // use the sort function as - menu_list_array.sort();
         htmldata=""
         for(var i=0;i<menu_list_array.length;i++){
-            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+            htmldata=htmldata+menu_list_array[i]+'<br>';
         }
          document.getElementById("display_addedmenu").innerHTML = htmldata;		
       }
